ca50e6a0b1c4a49d87345454d048875be5f23933
Andrian Cheniago <andrian.cheniago@students.mikroskil.ac.id>
2025-07-11 15:18:04 +0700

    menyambungkan my certif

--- lib/screens/my_certificate.dart
+++ lib/screens/my_certificate.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:projek_mobile/data/category.dart';
 import 'package:projek_mobile/data/my_course_data.dart';
 import 'package:projek_mobile/providers/theme_provider.dart';
+import 'package:projek_mobile/screens/certificate.dart';
 import 'package:projek_mobile/screens/explore_page.dart';
 import 'package:projek_mobile/widgets/category_chips.dart';
 import 'package:provider/provider.dart';
@@ -41,20 +42,17 @@ class _CertificatePageState extends State<CertificatePage> {
                   selectedIndexes.map((i) => categoryList[i - 1]).toList();
               return selectedCategories.contains(course.category);
             }).toList();
+
     final isDarkMode = Provider.of<ThemeNotifier>(context).isDarkMode;
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: isDarkMode ? Colors.black : Color(0xFF324EAF),
+        backgroundColor: isDarkMode ? Colors.black : const Color(0xFF324EAF),
         foregroundColor: Colors.white,
-        leading: Tooltip(
-          message: 'Back',
-          child: IconButton(
-            icon: const Icon(Icons.arrow_back, color: Colors.white),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () => Navigator.pop(context),
         ),
         title: Text(
           'My Certificate',
@@ -87,7 +85,7 @@ class _CertificatePageState extends State<CertificatePage> {
                               crossAxisCount: 2,
                               crossAxisSpacing: 16,
                               mainAxisSpacing: 16,
-                              childAspectRatio: 0.9,
+                              childAspectRatio: 0.7, // Perbaikan overflow
                             ),
                         itemCount: filteredCourses.length,
                         itemBuilder: (context, index) {
@@ -103,23 +101,40 @@ class _CertificatePageState extends State<CertificatePage> {
   }
 
   Widget _buildCertificateCard(course) {
-    return Card(
-      child: Container(
-        decoration: BoxDecoration(
-          color: const Color(0xFF324EAF),
-          borderRadius: BorderRadius.circular(10),
-        ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder:
+                (_) => const CertificateImageScreen(
+                  imagePath: 'assets/images/certificate.jpg',
+                ),
+          ),
+        );
+      },
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            ClipRRect(
-              borderRadius: const BorderRadius.vertical(
-                top: Radius.circular(10),
+            Expanded(
+              flex: 2,
+              child: ClipRRect(
+                borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(10),
+                ),
+                child: Image.asset(
+                  course.images,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
               ),
-              child: Image.asset(course.images, height: 150, fit: BoxFit.cover),
             ),
-            Padding(
-              padding: const EdgeInsets.all(6.0),
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              color: const Color(0xFF324EAF),
               child: Text(
                 course.title,
                 maxLines: 2,
